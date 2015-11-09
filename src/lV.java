71514827187b61f37fb2d18b612ac6629ca0837e
FA12-BSE-221 <FA12-BSE-221@LAB7C-13.cns.ciit>
2015-11-09 10:21:25 +0500

    changes occur

---
+++ src/lV.java
@@ -0,0 +1,10 @@
+
+public class lV {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("lets play with GIIIIIIIIIII");
+		System.out.println(" GIIIIIIIIIII");
+	}
+
+}
